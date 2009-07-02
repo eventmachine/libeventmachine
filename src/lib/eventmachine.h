3fb2d83a34bec764897da66c9cbb4159dfd2c666
dan sinclair <dj2@everburning.com>
2009-07-01 23:14:32 -0400

    add extern C

--- src/lib/eventmachine.h
+++ src/lib/eventmachine.h
@@ -1,8 +1,16 @@
 #ifndef EVENTMACHINE_H
 #define EVENTMACHINE_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int em_init(void);
 int em_shutdown(void);
 void em_run(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
